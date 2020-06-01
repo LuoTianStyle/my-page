c5c749703bcdf77604954e8b3f210db0e7dff59a
tianluoluo <tianluoluo666@gmail.com>
2020-06-01 23:04:36 +0800

    部署1

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
+echo '进入项目目录'
 cd /data/www
 git clean -f
 git reset --hard origin/master
+echo '拉取代码'
 git pull
+echo '安装'
 cnpm install
+echo '构建'
 npm run build
+echo '删除modules'
 rm -rf node_modules
+echo '完成'
