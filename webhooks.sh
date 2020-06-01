c357fac567b420d00935c26c789b1ca4af9e5cde
tian <tianluoluo666@gmail.com>
2020-06-01 11:03:46 +0800

    服务器配置

---
+++ webhooks.sh
@@ -0,0 +1,9 @@
+WEB_PATH='/data/wwww'
+echo "开始执行shell"
+cd $WEB_PATH
+echo "pulling source code..."
+git pull
+echo "changing permissions..."
+echo " git pull 完成. 开始 build"
+npm run build
+echo "build 完成"
