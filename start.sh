e291149cc59456ab84f6813b2cc98887e13f1222
tianluoluo <tianluoluo666@gmail.com>
2020-06-01 21:23:56 +0800

    配置

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
-## 进入博客目录
-echo '进入博客目录'
+
+echo '进入目录'
 cd /data/www
-## 拉取最新代码
 echo '开始拉去最新代码'
 git pull
-## 安装静态包
+echo '安装静态包'
 cnpm i
-## 打包静态文件
 echo '开始构建静态文件'
 npm run build
-echo '最新文章处理完成'
+echo '完成'
