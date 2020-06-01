057a583c943bf347a1a85f24d98ab18c17c80e49
tian <tianluoluo666@gmail.com>
2020-06-01 11:18:03 +0800

    测试

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
+const path = require('path');
 function resolve(dir) {
-    return path.join(__dirname, dir)
+	return path.join(__dirname, dir);
 }
 module.exports = {
-    chainWebpack: (config) => {
-        //修改文件引入自定义路径
-        config.resolve.alias
-            .set('@', resolve('src'))
-            .set('img', resolve('src/assets/img'))
-            .set('style', resolve('src/assets/style'))
-    },
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000/users/',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    }
-}
+	chainWebpack: config => {
+		//修改文件引入自定义路径
+		config.resolve.alias
+			.set('@', resolve('src'))
+			.set('img', resolve('src/assets/img'))
+			.set('style', resolve('src/assets/style'));
+	},
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000/users/',
+				ws: true,
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '',
+				},
+			},
+		},
+	},
+};
