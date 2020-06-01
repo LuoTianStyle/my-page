376b95530a060437cb67dd00044de721590e2f2f
tianluoluo <tianluoluo666@gmail.com>
2020-06-01 20:53:53 +0800

    配置

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-var http = require('http');
-var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/', secret: '123456' });
+var http = require("http");
+var createHandler = require("/usr/local/node-v12/bin/github-webhook-handler");
+var handler = createHandler({ path: "/", secret: "123456" });
 
 function RunCmd(cmd, args, cb) {
-	var spawn = require('child_process').spawn;
-	var child = spawn(cmd, args);
-	var result = '';
-	child.stdout.on('data', function(data) {
-		result += data.toString();
-	});
-	child.stdout.on('end', function() {
-		cb(result);
-	});
+    var spawn = require("child_process").spawn;
+    var child = spawn(cmd, args);
+    var result = "";
+    child.stdout.on("data", function(data) {
+        result += data.toString();
+    });
+    child.stdout.on("end", function() {
+        cb(result);
+    });
 }
 
 http.createServer(function(req, res) {
-	handler(req, res, function(err) {
-		res.statusCode = 404;
-		res.end('no such location');
-	});
+    handler(req, res, function(err) {
+        res.statusCode = 404;
+        res.end("no such location");
+    });
 }).listen(3000);
 
-handler.on('error', function(err) {
-	console.error('Error:', err.message);
+handler.on("error", function(err) {
+    console.error("Error:", err.message);
 });
 
-handler.on('push', function(event) {
-	console.log(
-		'Received a push event for %s to %s',
-		event.payload.repository.name,
-		event.payload.ref
-	);
-	var shpath = './start.sh';
-	RunCmd('sh', [shpath], function(result) {
-		console.log(result);
-	});
+handler.on("push", function(event) {
+    console.log(
+        "Received a push event for %s to %s",
+        event.payload.repository.name,
+        event.payload.ref
+    );
+    var shpath = "./start.sh";
+    RunCmd("sh", [shpath], function(result) {
+        console.log(result);
+    });
 });
 
-handler.on('issues', function(event) {
-	console.log(
-		'Received an issue event for %s action=%s: #%d %s',
-		event.payload.repository.name,
-		event.payload.action,
-		event.payload.issue.number,
-		event.payload.issue.title
-	);
+handler.on("issues", function(event) {
+    console.log(
+        "Received an issue event for %s action=%s: #%d %s",
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title
+    );
 });
